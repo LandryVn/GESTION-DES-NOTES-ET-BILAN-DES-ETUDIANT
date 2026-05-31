31d3c2abc5a57b2b6c969c6c35c5173930452e76
LandryVN <landryvoavy@email.com>
2026-05-31 13:19:45 +0300

    second commit

--- Projet-reactjs/frontend/src/components/Ajouter.jsx
+++ Projet-reactjs/frontend/src/components/Ajouter.jsx
@@ -68,7 +68,7 @@ export default function AjouterSimple() {
           />
         </div>
   
-        <div>
+        {/* <div>
           <label className="block text-sm font-medium text-gray-700 mb-1">
             Nom Complet:
           </label>
@@ -79,7 +79,7 @@ export default function AjouterSimple() {
             className="w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
             required
           />
-        </div>
+        </div> */}
   
         <div className="grid grid-cols-2 gap-4">
           <div>
